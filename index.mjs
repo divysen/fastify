0429d1df7d23b5b265d736d965cd4933766ed2c4
divy sen <divysen.astro@gmail.com>
2022-02-07 19:36:50 +0530

    bugfix: changed to env port

--- index.mjs
+++ index.mjs
@@ -1,13 +1,14 @@
 import Fastify from "fastify";
 import PublicRoutes from "./routes/public.js";
 import AdminRoutes from "./routes/admin.js";
+let port = process.env.port || 2100;
 
 const fastify = Fastify({ logger: true });
 
 fastify.register(PublicRoutes, { prefix: '/' });
 fastify.register(AdminRoutes, { prefix: '/admin' });
 
-fastify.listen(2100, function (err, address) {
+fastify.listen(port, function (err, address) {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
